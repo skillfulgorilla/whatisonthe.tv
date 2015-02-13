690c0cb69515f0f01c84022fad96c95c232ded75
Stephen McCullough <stephen@skillfulgorilla.com>
2015-02-13 19:19:56 +0000

    #5 - Added coverage and nose

--- listings/test.py
+++ listings/test.py
@@ -2,4 +2,16 @@ from settings import *
 
 DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3', 'NAME': 'sfp.sqlite3'}
 
+INSTALLED_APPS += (
+    'django_nose',
+) 
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=tv',
+    '--cover-html',
+]
+
 XML_TV_LISTING_FEED = "tv/tests/fixtures/tv.xml"
