a9acd378f8743341cea3e88d363c95e5b8efb702
Stephen McCullough <stephen@skillfulgorilla.com>
2015-02-16 17:41:32 +0000

    #15 some coverage configs

--- listings/test.py
+++ listings/test.py
@@ -1,4 +1,9 @@
 from settings import *
+from django.conf import settings
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+TESTING = True
 
 DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3', 'NAME': 'sfp.sqlite3'}
 
@@ -6,7 +11,10 @@ INSTALLED_APPS += (
     'django_coverage',
 ) 
 
+COVERAGE_USE_CACHE = getattr(settings, 'COVERAGE_USE_CACHE', False)
 COVERAGE_REPORT_HTML_OUTPUT_DIR = 'cover'
+COVERAGE_USE_STDOUT = True 
+COVERAGE_MODULE_EXCLUDES = getattr(settings, 'COVERAGE_MODULE_EXCLUDES', ['tests$', 'settings$', 'urls$', 'locale$', 'common.views.test', '__init__', 'django', 'migrations'])
 
 TEST_RUNNER = 'django_coverage.coverage_runner.CoverageRunner'
 
