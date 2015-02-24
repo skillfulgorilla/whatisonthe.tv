85e0a4b5db12a3aee410cdccd45df9b5f13d6dfd
Stephen McCullough <stephen@skillfulgorilla.com>
2015-02-24 08:32:20 +0000

    #11 - Remove trailing whitespace

--- tv/models.py
+++ tv/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class BroadCast(models.Model):
 
-    programme = models.ForeignKey('Programme', blank=True, null=True) 
+    programme = models.ForeignKey('Programme', blank=True, null=True)
     channel = models.ForeignKey('Channel', blank=True, null=True)
     start_time = models.DateTimeField(auto_now=False, auto_now_add=False)
     end_time = models.DateTimeField(auto_now=False, auto_now_add=False)
-    blurb = models.TextField(blank=True)   
+    blurb = models.TextField(blank=True)
 
     def __unicode__(self):
         return "%s %s" % (self.start_time, self.end_time)
